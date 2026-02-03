6a97f9456f79ab617078bab9b64b702f456e18b3
Xenaphobiaaa <dklimova71@gmail.com>
2026-02-03 14:02:22 +0300

    feat: add Core project with TaskItem model

---
+++ TaskTracker/TaskTracker.Core/Models/TaskItem.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Core.Models;
+
+public class TaskItem
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = "";
+    public TaskStatus Status { get; set; } = TaskStatus.New;
+}
---
+++ TaskTracker/TaskTracker.Core/Models/TaskStatus.cs
@@ -0,0 +1,8 @@
+﻿namespace TaskTracker.Core.Models;
+
+public enum TaskStatus
+{
+    New = 0,
+    InProgress = 1,
+    Done = 2
+}
